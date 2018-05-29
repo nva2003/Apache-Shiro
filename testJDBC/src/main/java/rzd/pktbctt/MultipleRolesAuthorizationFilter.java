5e519cad70690b0bc69f43999f6db72fb4aafcbb
nva <nva@yandex.ru>
2018-05-29 08:30:21 +0300

    test JDBC

---
+++ testJDBC/src/main/java/rzd/pktbctt/MultipleRolesAuthorizationFilter.java
@@ -0,0 +1,37 @@
+package rzd.pktbctt;
+
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.web.filter.authz.RolesAuthorizationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * User: VNikishin
+ * Date: 28.05.18
+ * Time: 16:35
+ */
+public class MultipleRolesAuthorizationFilter extends RolesAuthorizationFilter {
+
+    @Override
+    public boolean isAccessAllowed(ServletRequest request, ServletResponse response,
+                                   Object mappedValue) throws IOException {
+
+        final Subject subject = getSubject(request, response);
+        final String[] rolesArray = (String[]) mappedValue;
+
+        if (rolesArray == null || rolesArray.length == 0) {
+            return true;
+        }
+
+        for (String roleName : rolesArray) {
+            if (subject.hasRole(roleName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
